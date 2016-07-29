836036f22d7b35dda132f35f613deb561c1900b2
dnoc <drew.noct@gmail.com>
2016-07-28 20:17:05 -0400

    Field Test, green

--- src/main/java/com/audition/PacManGame.java
+++ src/main/java/com/audition/PacManGame.java
@@ -2,5 +2,27 @@ package com.audition;
 
 
 public class PacManGame {
+    String[][] field;
 
+    PacManGame() {
+        field = initializeField();
+    }
+
+    private String[][] initializeField() {
+        int x = 3;
+        int y = 3;
+        field = new String[x][y];
+        while (x > 0) {
+            while (y > 0) {
+                if ((x == 2) && (y == 2)) {
+                    field[x-1][y-1] = "V";
+                } else {
+                    field[x-1][y-1] = ".";
+                }
+                y--;
+            }
+            x--;
+        }
+        return field;
+    }
 }
